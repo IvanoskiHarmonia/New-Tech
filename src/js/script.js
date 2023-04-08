cfa26f94b9eb20ead3735037014e150d88ca7aad
Martin Ivanoski <martinivanoski@Martins-MacBook-Pro.local>
2023-04-07 17:39:26 -0700

    Version-1: little fix on the styling and colors

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos && document.getElementById("navbar").style.top != "0") {
+    if (currentScrollPos > 20 && prevScrollpos > currentScrollPos && document.getElementById("navbar").style.top != "0") {
         document.getElementById("navbar").style.top = "0";
-    } else if (prevScrollpos < currentScrollPos && document.getElementById("navbar").style.top != "-80px") {
+    } else if (currentScrollPos > 20 && prevScrollpos < currentScrollPos && document.getElementById("navbar").style.top != "-80px") {
         document.getElementById("navbar").style.top = "-80px";
     }
     prevScrollpos = currentScrollPos;
